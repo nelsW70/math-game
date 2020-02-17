466dc57043651faf7feba58fd760163dabb68aa4
Foltz <NFoltz@ppg.com>
2020-02-17 15:31:50 -0500

    cleanup

--- src/app/answer-highlight.directive.ts
+++ src/app/answer-highlight.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
 import { NgControl } from '@angular/forms';
-import { map, filter } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Directive({
   selector: '[appAnswerHighlight]'
