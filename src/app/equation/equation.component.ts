c3055c977d36873ecfee27dc24af75228cdb2a2b
Foltz <NFoltz@ppg.com>
2020-02-17 13:09:00 -0500

    add: resetting value on randomly generated numbers using setValue  once correct answer is provided

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -29,7 +29,12 @@ export class EquationComponent implements OnInit {
 
   ngOnInit() {
     this.mathForm.statusChanges.subscribe(value => {
-      console.log('value', value);
+      if (value === 'INVALID') {
+        return;
+      }
+      this.mathForm.controls.a.setValue(this.randomNumber());
+      this.mathForm.controls.b.setValue(this.randomNumber());
+      this.mathForm.controls.answer.setValue('');
     });
   }
 
