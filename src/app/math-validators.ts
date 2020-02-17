8d74f0524e7740e230c77d904454c22a32932a0a
Foltz <NFoltz@ppg.com>
2020-02-17 11:08:10 -0500

    add: created static method for custom validator and moved it from equation ts to MathValidators

--- src/app/math-validators.ts
+++ src/app/math-validators.ts
@@ -1,1 +1,13 @@
-export class MathValidators {}
+import { AbstractControl } from '@angular/forms';
+
+export class MathValidators {
+  static addition(form: AbstractControl) {
+    const { a, b, answer } = form.value;
+    // tslint:disable-next-line: radix
+    if (a + b === parseInt(answer)) {
+      return null;
+    }
+
+    return { addition: true };
+  }
+}
